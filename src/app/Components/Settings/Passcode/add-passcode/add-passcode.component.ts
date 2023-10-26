992293b6606b727c102c81d35bd032e6939496d8
kishor <iamkishorshrestha@gmail.com>
2023-10-25 21:13:54 -0400

    na

--- src/app/Components/Settings/Passcode/add-passcode/add-passcode.component.ts
+++ src/app/Components/Settings/Passcode/add-passcode/add-passcode.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-add-passcode',
   templateUrl: './add-passcode.component.html',
-  styleUrls: ['./add-passcode.component.scss']
+  styleUrls: ['./add-passcode.component.scss'],
 })
-export class AddPasscodeComponent {
+export class AddPasscodeComponent implements OnInit {
+  constructor(
+    private _fb: FormBuilder,
+    // private _dialogRef: MatDialogRef<AddPasscodeComponent>
+    @Inject(MAT_DIALOG_DATA) public _data: any
+  ) {}
 
+  passcodeForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.passcodeForm = this._fb.group({
+      passcode: ['', Validators.required],
+    });
+  }
 }
